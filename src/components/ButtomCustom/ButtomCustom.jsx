5a99eef5cd92d15632d2bf7e9c980cb154777505
Vladyslav Bezkorovainyi <seile6558@gmail.com>
2023-08-21 10:41:18 +0300

    D

--- src/components/ButtomCustom/ButtomCustom.jsx
+++ src/components/ButtomCustom/ButtomCustom.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 export default function ButtomCustom({handleFind,text}) {
   return (
     <button onClick={handleFind}
-    type="button" class="text-gray-900
+    type="button" class="
      hover:text-white border border-gray-800
       hover:bg-gray-900 focus:ring-4 focus:outline-none
        focus:ring-gray-300 font-medium rounded-lg text-sm 
-       px-5 py-2.5 text-center mr-2 mb-2
+       px-5 py-2.5 text-center mr-2 mb-2 text-white
        
       ">
          {text}
