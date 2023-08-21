22f74d0a89d3a945dfc4e481d46fe9ac07f40375
Vladyslav Bezkorovainyi <seile6558@gmail.com>
2023-08-21 10:42:48 +0300

    D

--- src/components/ButtomCustom/ButtomCustom.jsx
+++ src/components/ButtomCustom/ButtomCustom.jsx
@@ -4,7 +4,7 @@ export default function ButtomCustom({handleFind,text}) {
   return (
     <button onClick={handleFind}
     type="button" class="
-     hover:text-white border border-gray-800
+     hover:text-white border border-white
       hover:bg-gray-900 focus:ring-4 focus:outline-none
        focus:ring-gray-300 font-medium rounded-lg text-sm 
        px-5 py-2.5 text-center mr-2 mb-2 text-white
